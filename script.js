3bf394dff86cd3da854aac1604a0cafa0541e1b5
MDIE2202 <marty.diender@gmail.com>
2022-08-11 22:17:13 +0200

    added a reset button and a function that changes the color to a random RGB color

--- script.js
+++ script.js
@@ -16,12 +16,39 @@ grid = (size) => {
     main_container = document.querySelector('.main_container')
     main_container.appendChild(column);
 
-
 }
 
 grid(100)
 
+rand_color = () =>{
+    let rand = Math.floor(Math.random()*255);
+    let rand2 = Math.floor(Math.random()*255);
+    let rand3 = Math.floor(Math.random()*255);
+    color = `rgb(${rand},${rand2},${rand3})`
+    return color
+}
 
+user_input = () => {
+    user = prompt('Enter a grid size between 10 and 100:')
+    if(user > 100){
+        user = 100;
+    }
+    else if(user < 10){
+        user = 10;
+    }
+    else{}
+
+    const clean = document.querySelector('.main_container')
+    clean.innerHTML = '';
+    grid(user);
+    hover_actions = document.querySelectorAll('.box');
+    hover_actions.forEach(box => box.addEventListener('mouseover', function(){this.style.backgroundColor = rand_color()}))   
+
+}
 
 hover_actions = document.querySelectorAll('.box');
-hover_actions.forEach(box => box.addEventListener('mouseover', function(){this.classList.add('color_change')}))
+hover_actions.forEach(box => box.addEventListener('mouseover', function(){this.style.backgroundColor = rand_color()}))
+
+set_grid = document.querySelector('.set');
+set_grid.addEventListener('click', function(){user_input()});
+
